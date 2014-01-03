72812e3152731455afb50f310489473011d2ae3a
Mohan Zhang <mohanzhang@gmail.com>
2014-01-03 12:47:33 -0500

    Generate new project, riding rails 4.0.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dappernews::Application.config.secret_key_base = 'aeeb48677c0cd0e90d5884bb8c8d672fbc9ea38f13f93b5b8c418bab157485ab51afe81b044e763e075ab7a46a749ad4f312677a751e9ef6eb81ddbedefbb693'
