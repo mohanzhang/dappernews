429f31a9564458202f57fea5520fedc73d0e1e7b
Mohan Zhang <mohanzhang@gmail.com>
2014-01-03 19:31:16 -0500

    Establish model associations and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
-Item.create(title: "Video of cat doing things cats actually do, like sit around",
-            url: "http://example.com/cats",
-            username: "mohanzhang")
+i = Item.create(title: "Video of cat doing things cats actually do, like sit around",
+                url: "http://example.com/cats",
+                username: "mohanzhang")
+
+c = i.comments.create(username: "mr_t",
+                      content: "This was really funny")
